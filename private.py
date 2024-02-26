921cb485b2050f4c61c7fa357bf7b1056bb64cef
v1aD0S34 <144205052+v1aD0S34@users.noreply.github.com>
2024-02-26 11:51:43 +0300

    data

---
+++ private.py
@@ -0,0 +1,11 @@
+TOKEN = "your_TOKEN"
+
+PHOTO = {
+    "pepa": "https://preview.redd.it/bd7q2jvcght21.jpg?auto=webp&s=f36a77a9f383441047a7e3b3f8fa88870d2f700f",
+}
+
+URL = {
+    "vega": "https://www.vega-gaz.ru/",
+    "clickApp": "https://0c90-85-91-122-58.ngrok-free.app",
+    "ipAPI": "https://api.ipify.org?format=json"
+}
